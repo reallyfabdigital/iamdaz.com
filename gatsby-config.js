9a98bf23095f12374795c42988312854e04f2f05
Darren <48988488+reallyfabdigital@users.noreply.github.com>
2020-05-15 19:04:53 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `iamdaz.com`,
+    author: `Darren Griffin`,
+    description: `A personal blog based on Gatsby and hoted with Netlify CMS.`,
+    siteUrl: `https://iamdaz.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `darrenpgriffin`,
     },
   },
   plugins: [
