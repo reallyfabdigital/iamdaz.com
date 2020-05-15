60b572eb902cc0a728a19b4b13dffe0790a3bcea
Darren <48988488+reallyfabdigital@users.noreply.github.com>
2020-05-15 18:07:12 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "hello, i am daz"
+    const siteTitle = "hello. i am daz."
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
