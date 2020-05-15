7b8c7c0d0bbdc311c4071b1dfb8e6b38a10dd2e5
Darren <48988488+reallyfabdigital@users.noreply.github.com>
2020-05-15 18:31:16 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "iamdaz.com"
+    const siteTitle = "Hola!"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -16,9 +16,9 @@ class IndexPage extends React.Component {
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <img style={{ margin: 0 }} src="./bender-over-there-compressor.png" alt="Welcome to iamdaz.com" />
-        <h2>
+        <h1>
           Sometimes the most clever thing to say is nothing at all.
-        </h2>
+        </h1>
         <p>Welcome to my website. It's a bit of an experiment, so bear with. 
       I built it mainly as an excuse to fiddle with Gatsby, github and Netlify CMS and am still fiddling. So it may break.</p>
         <p>
